69e2bea20461f7b7d106a5147b0b67e4116a666d
duythvo <vothaiduy19092004@gmail.com>
2024-05-01 16:58:11 +0700

    fix SuatChieu_DAO

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -1,6 +1,8 @@
 package Test;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 
 import DAO.LoaiPhim_DAO;
@@ -9,28 +11,19 @@ import DAO.SuatChieu_DAO;
 import connectDB.ConnectDB;
 import entity.LoaiPhim;
 import entity.Phim;
+import entity.PhongChieu;
 import entity.SuatChieu;
 
 public class Main {
-    // public static void main(String[] args) {
-    //     try {
-    //         ConnectDB.getInstance().connect();
-    //     } catch (SQLException e) {
-    //         e.printStackTrace();
-    //     }
+    public static void main(String[] args) {
+        try {
+            ConnectDB.getInstance().connect();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
-      
-    //     Phim_DAO phim_dao = new Phim_DAO();
-
-       
-
-    //     if( phim_dao.themPhim(new Phim("P06", "Phim 1", "Dao Dien 1", "Quoc Gia 1", 120, new LoaiPhim("LP01")))){
-    //         System.out.println("Them thanh cong");
-    //     }
-    //     else{
-    //         System.out.println("Them that bai");
-    //     }
-        
-    //      ConnectDB.getInstance().disconnect();
-    // }
+        SuatChieu_DAO suatChieu_dao = new SuatChieu_DAO();
+        suatChieu_dao.themSuatChieu(new SuatChieu("SC03",LocalDate.now(), LocalTime.now(), new Phim("P01"), new PhongChieu("PC01")));
+        //ArrayList<SuatChieu> dsSuatChieu = suatChieu_dao.getDSPhim();
+    }
 }
